337d561b84f2f3ed449e34a7b4b57b78927032e5
xuwangdong <xuwangdong@boohee.com>
2022-08-24 13:57:03 +0800

    feat: 侦听器  watch 尝试

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <template>
   <div>
-    <h2>{{ getFullName() }}</h2>
-    <h2>{{ getScore() }}</h2>
-    <h2>{{ getScore() }}</h2>
-    <h2>{{ getReverseMessage() }}</h2>
+    请输入问题：
+    <input type="text"  v-model="question" />
 
-    <!-- 计算属性 -->
-    <h2>{{ fullName }}</h2>
-    <h2>{{ score }}</h2>
-    <h2>{{ score }}</h2>
-    <h2>{{ reverseMessage }}</h2>
-
-    <button @click="setNewName">设置新名字</button>
+    <div>{{info.name}}</div>
+    <button @click="btnClick">修改 info name</button>
   </div>
 </template>
 
@@ -19,47 +12,40 @@
   export default {
     data() {
       return {
-        firstName:'xu',
-        lastName:'corey',
-        score:99,
-        message:'hello world'
+        question:'',
+        info: {
+          name:'corey'
+        }
       }
     },
-    methods: {
-      setNewName() {
-        this.fullName = 'corey code'
-      },
-      getFullName() {
-        return `${this.firstName} ${this.lastName}`
-      },
-      getScore() {
-        console.log("调用了计算属性result的getter");
-        return this.score >= 60 ? '及格' : '不及格'
-      },
-      getReverseMessage() {
-        return this.message.split(' ').reverse().join(' ')
-      }
-    },
-    computed:{
-      // fullName() {
-      //   return `${this.firstName} ${this.lastName}`
-      // },
-      fullName: {
-        get() {
-          return `${this.firstName} ${this.lastName}`
-        },
-        set(value) {
-          const names = value.split(" ");
-          this.firstName = names[0];
-          this.lastName = names[1];
+    watch: {
+      question(newVal, oldVal) {
+        console.log('newVal--',newVal,'oldVal--',oldVal);
+        if(newVal === oldVal) {
+          console.log('请后值一致');
+        }else {
+          this.getAnswer(newVal);
         }
       },
-      score() {
-        console.log("调用了computed方法");
-        return this.score >= 60 ? '及格' : '不及格'
+      // 默认情况下，watch只是在侦听info的引用变化，对于内部属性的变化是不会做出相应的：
+      // info(newVal, oldVal) {
+      //   console.log('newVal--',newVal,'oldVal--',oldVal);
+      // }
+      info:{
+        handler(newVal, oldVal) {
+          console.log('------');
+          // console.log('newVal--',newVal.name,'oldVal--',oldVal.name);
+        },
+        deep:true,
+        immediate: true // 一开始就立即执行一次
+      }
+    },
+    methods:{
+      getAnswer(val) {
+        console.log(`${val}的问题答案是没有答案`);
       },
-      reverseMessage() {
-        return this.message.split(' ').reverse().join(' ')
+      btnClick() {
+        this.info.name = 'coco'
       }
     }
   }
