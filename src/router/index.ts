f6b7bab271aa07fba7b3fe0bf7da019b723c5f6d
xuwangdong <xuwangdong@boohee.com>
2022-10-14 15:06:51 +0800

    feat: 路由配置修改

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,10 +2,13 @@
 import { createRouter, createWebHashHistory, createWebHistory, RouteRecordRaw, RouterOptions } from "vue-router"
 
 import list, { RouterObj } from "./routerList"
+const modules = import.meta.glob('../views/**/*.vue')
 
 const getRoutes = (list:RouterObj[]):RouteRecordRaw[] => {
  return list.map(item => {
-    const component = () => import(`/src/views/${item.src}/index.vue`);
+    const component = modules[`../views/${item.src}/index.vue`];
+     
+    // const component = () => import(/* @vite-ignore */ `src/views/${item.src}/index.vue`);
     return {
       path: item.path,
       component: component,
@@ -14,12 +17,12 @@ const getRoutes = (list:RouterObj[]):RouteRecordRaw[] => {
   })
 }
 
-const routes1 = getRoutes(list);
+const routes = getRoutes(list);
 
 
 const router = createRouter({
   history: createWebHistory(),
-  routes:routes1
+  routes:routes
 })
 
 export default router
